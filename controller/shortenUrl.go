13e0a4b5773279e4101f40fc6aaf8887d7389308
LiuZichen <danteliuzichen@163.com>
2019-04-27 11:44:19 +0800

    '加入数据库'

--- controller/shortenUrl.go
+++ controller/shortenUrl.go
@@ -17,6 +17,6 @@ func ShortenUrl(c *gin.Context) {
 
 func GetOriginUrl(c *gin.Context) {
 	url := c.Params.ByName("url")
-	res, ok := model.SearchOriginUrl(url)
+	res, ok := model.SearchOriginUrlSQL(url)
 	c.JSON(http.StatusOK, gin.H{"url": res, "success": ok})
 }
--- model/shortenUrl.go
+++ model/shortenUrl.go
@@ -1,23 +1,39 @@
 package model
 
+import (
+	. "Url-Shortener/model/base"
+)
+
 var db = make(map[string]string)
 var reverseDb = make(map[string]string)
 
+type urlPair struct {
+	originUrl string
+	shortUrl  string
+}
+
 func ShortenUrl(longUrl string) string {
-	if shortUrl, ok := SearchShortUrl(longUrl); ok{
+	if shortUrl, ok := SearchShortUrlSQL(longUrl); ok{
 		return shortUrl
 	}
-	shortUrl := getShortUrl(longUrl)
-	db[longUrl] = shortUrl
-	reverseDb[shortUrl] = longUrl
-	return shortUrl
+	var urlP urlPair
+	urlP.shortUrl = getShortUrl(longUrl)
+	urlP.originUrl = longUrl
+	insertUrlSQL(urlP)
+	//db[longUrl] = shortUrl
+	//reverseDb[shortUrl] = longUrl
+	return urlP.shortUrl
+}
+
+func insertUrlSQL(urlP urlPair) {
+	Db.Exec("insert INTO url(origin_url, short_url) values(?,?)", urlP.originUrl, urlP.shortUrl)
 }
 
 func getShortUrl(longUrl string) string {
 	shortUrlList, _ := Transform(longUrl)
 	var res string
 	for _, v := range shortUrlList{
-		if _, ok := SearchOriginUrl(v); !ok{
+		if _, ok := SearchOriginUrlSQL(v); !ok{
 			res = v
 			break
 		}
@@ -28,12 +44,25 @@ func getShortUrl(longUrl string) string {
 	return res
 }
 
-func SearchShortUrl(longUrl string) (string, bool) {
-	shortUrl, ok := db [longUrl]
-	return shortUrl, ok
+func SearchShortUrlSQL(longUrl string) (string, bool) {
+	// shortUrl, ok := db [longUrl]
+	urlP := new(urlPair)
+	row := Db.QueryRow("select short_url from url where origin_url = ?", longUrl)
+	if err := row.Scan(&urlP.shortUrl); err == nil{
+		if urlP.shortUrl != ""{
+			return urlP.shortUrl, true
+		}
+	}
+	return "", false
 }
 
-func SearchOriginUrl(shortUrl string) (string, bool) {
-	originUrl, ok := reverseDb [shortUrl]
-	return originUrl, ok
+func SearchOriginUrlSQL(shortUrl string) (string, bool) {
+	urlP := new(urlPair)
+	row := Db.QueryRow("select origin_url from url where short_url = ?", shortUrl)
+	if err := row.Scan(&urlP.originUrl); err == nil{
+		if urlP.originUrl != ""{
+			return urlP.originUrl, true
+		}
+	}
+	return "", false
 }
